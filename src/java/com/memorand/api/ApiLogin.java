f207f6993f4d084486fbd2bb783378fb881a0a9a
nicojrz <juarez.barrios.axel.nicolas@gmail.com>
2024-03-07 00:23:07 -0600

    memorand 3.0.26

---
+++ src/java/com/memorand/api/ApiLogin.java
@@ -0,0 +1,46 @@
+package com.memorand.api;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ApiLogin extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet ApiLogin</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet ApiLogin at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+
+}
---
+++ src/java/com/memorand/api/ApiUserInfo.java
@@ -0,0 +1,55 @@
+package com.memorand.api;
+
+import com.google.gson.Gson;
+import com.memorand.beans.User;
+import com.memorand.controller.UsersController;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ApiUserInfo extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("application/json");
+        
+        String user_id = request.getParameter("user_id");
+        
+        if (user_id == null || user_id.isEmpty())
+        {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        }
+        
+        UsersController usersc = new UsersController();
+        User user = usersc.modelGetUserInfoById(user_id);
+        
+        Gson gson = new Gson();
+        
+        String user_info = gson.toJson(user);
+        
+        try (PrintWriter out = response.getWriter()) {
+            out.println(user_info);
+        }
+        
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+
+}
