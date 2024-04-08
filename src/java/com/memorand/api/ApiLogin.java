1193c8b6ad6873ecef5e4b9139e20b9e8c1866d7
gengram <mygengram@gmail.com>
2024-04-07 22:44:55 -0600

    Api login

--- src/java/com/memorand/api/ApiLogin.java
+++ src/java/com/memorand/api/ApiLogin.java
@@ -1,5 +1,7 @@
 package com.memorand.api;
 
+import com.memorand.beans.User;
+import com.memorand.controller.UsersController;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -9,38 +11,31 @@ import javax.servlet.http.HttpServletResponse;
 
 public class ApiLogin extends HttpServlet {
 
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ApiLogin</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ApiLogin at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
+            throws ServletException, IOException
+    {
+        response.setContentType("text/plain;charset=UTF-8");
+        
+        try (PrintWriter out = response.getWriter())
+        {
+            String user_email = request.getParameter("user_email");
+            String user_pass = request.getParameter("user_pass");
+            
+            UsersController userc = new UsersController();
+            User user = new User(user_email, user_pass);
+            
+            if (userc.modelLoginUser(user))
+            {
+                response.setStatus(200);
+            }
+            else
+            {
+                response.setStatus(400);
+            }
+            
+        }
     }
-
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }
-
+    
+    
 }
