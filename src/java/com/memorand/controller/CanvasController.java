2b1bba3502e383672ffa13278410c479041f7d62
gengram <mygengram@gmail.com>
2024-04-09 01:00:33 -0600

    Task controllers

--- src/java/com/memorand/controller/CanvasController.java
+++ src/java/com/memorand/controller/CanvasController.java
@@ -2,6 +2,9 @@ package com.memorand.controller;
 
 import com.memorand.beans.Canva;
 import com.memorand.model.CanvasModel;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class CanvasController
 {
@@ -23,6 +26,51 @@ public class CanvasController
                 + "            <h1><a href='canvanew.jsp?id=" + task_id + "'>+</a></h1>\n"
                 + "            \n"
                 + "            <h3>Lienzos del equipo</h3>";
+        
+        CanvasModel canvam = new CanvasModel();
+        ArrayList<Canva> canvas = canvam.getCanvasByTask(task_id);
+        
+        if (canvas.isEmpty())
+        {
+            htmlcode += "<p>No hay lienzos por mostrar</p>";
+            return htmlcode;
+        }
+        else
+        {
+            htmlcode += "<table border=\"1\">\n" +
+"                <thead>\n" +
+"                    <tr>\n" +
+"                        <th></th>\n" +
+"                        <th>Nombre</th>\n" +
+"                        <th>Creado el:</th>\n" +
+"                        <th>&Uacute;ltima modificaci&oacute;n</th>\n" +
+"                        <th></th>\n" +
+"                    </tr>\n" +
+"                </thead>\n" +
+"                <tbody>";
+            
+            for (Canva c : canvas)
+            {
+                String canva_id = c.getCanva_id();
+                
+                SimpleDateFormat sdf1 = new SimpleDateFormat("dd 'de' MMMM", new Locale("es"));
+                SimpleDateFormat sdf2 = new SimpleDateFormat("dd 'de' MMMM 'a la(s):' hh:mm a", new Locale("es"));
+                
+                String canva_cdate = sdf1.format(c.getCanva_cdate());
+                String canva_mdate = sdf2.format(c.getCanva_mdate());
+                
+                htmlcode += "<tr>\n" +
+"                        <td>:</td>\n" +
+"                        <td>"+ c.getCanva_name() +"</td>\n" +
+"                        <td>"+ canva_cdate +"</td>\n" +
+"                        <td>"+ canva_mdate +"</td>\n" +
+"                        <td><a href='nota.jsp?id="+ canva_id +"'>Ver</a></td>\n" +
+"                    </tr>";
+            }
+            
+            htmlcode += "</tbody>\n" +
+"            </table>";
+        }
         
         return htmlcode;
     }
--- src/java/com/memorand/controller/NotesController.java
+++ src/java/com/memorand/controller/NotesController.java
@@ -2,6 +2,9 @@ package com.memorand.controller;
 
 import com.memorand.beans.Note;
 import com.memorand.model.NotesModel;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class NotesController
 {
@@ -23,6 +26,51 @@ public class NotesController
                 + "            <h1><a href='notenew.jsp?id=" + task_id + "'>+</a></h1>\n"
                 + "            \n"
                 + "            <h3>Notas del equipo</h3>";
+        
+        NotesModel notem = new NotesModel();
+        ArrayList<Note> notes = notem.getNotesByTask(task_id);
+        
+        if (notes.isEmpty())
+        {
+            htmlcode += "<p>No hay notas por mostrar</p>";
+            return htmlcode;
+        }
+        else
+        {
+            htmlcode += "<table border=\"1\">\n" +
+"                <thead>\n" +
+"                    <tr>\n" +
+"                        <th></th>\n" +
+"                        <th>Nombre</th>\n" +
+"                        <th>Creada el:</th>\n" +
+"                        <th>&Uacute;ltima modificaci&oacute;n</th>\n" +
+"                        <th></th>\n" +
+"                    </tr>\n" +
+"                </thead>\n" +
+"                <tbody>";
+            
+            for (Note n : notes)
+            {
+                String note_id = n.getNote_id();
+                
+                SimpleDateFormat sdf1 = new SimpleDateFormat("dd 'de' MMMM", new Locale("es"));
+                SimpleDateFormat sdf2 = new SimpleDateFormat("dd 'de' MMMM 'a la(s):' hh:mm a", new Locale("es"));
+                
+                String note_cdate = sdf1.format(n.getNote_cdate());
+                String note_mdate = sdf2.format(n.getNote_mdate());
+                
+                htmlcode += "<tr>\n" +
+"                        <td>:</td>\n" +
+"                        <td>"+ n.getNote_name() +"</td>\n" +
+"                        <td>"+ note_cdate +"</td>\n" +
+"                        <td>"+ note_mdate +"</td>\n" +
+"                        <td><a href='nota.jsp?id="+ note_id +"'>Ver</a></td>\n" +
+"                    </tr>";
+            }
+            
+            htmlcode += "</tbody>\n" +
+"            </table>";
+        }
         
         return htmlcode;
     }
